2da3ee6ed326840c37121889c19b2681efaa10ea
Sergey Bastrikov <sergeywhitedev@gmail.com>
2022-07-29 13:45:34 +0500

    feat: different types of payment and accept command-line arguments

--- creditcalc.py
+++ creditcalc.py
@@ -1,4 +1,6 @@
-from math import ceil, pow, log
+from math import ceil, pow, log, floor
+import argparse
+from sys import argv
 
 
 def annuity_payment(p: int, n: int, i: float) -> int:
@@ -22,7 +24,7 @@ def loan_principal(a: float, n: int, i: float) -> int:
     :return p: Loan principal
     """
     p = a / (i * pow((1 + i), n) / (pow((1 + i), n) - 1))
-    return ceil(p)
+    return floor(p)
 
 
 def number_payments(p: int, a: int, i: float) -> int:
@@ -63,48 +65,106 @@ def singular_plural(num: int, word: str) -> str:
     return word + 's'
 
 
-type_ = input("""What do you want to calculate?
-type "n" - for number of monthly payments,
-type "a" for annuity monthly payment amount,
-type "p" - for loan principal: """)
+def differentiated_payment(p: int, n: int, i: float) -> list:
+    """
+    Returns the calculating differentiated payments
+    :param p: Loan principal
+    :param n: Number of payments. This is usually the number of months in which repayments will be made
+    :param i: Nominal interest rate
+    :return: Calculating differentiated payments
+    """
+    return [ceil(p / n + (i * (p - (p * (k - 1) / n)))) for k in range(1, n + 1)]
 
-# calculating the number of monthly payments
-if type_ == 'n':
-    p_ = int(input("Enter the loan principal: "))
-    a_ = int(input("Enter the monthly payment: "))
-    i_ = float(input("Enter the loan interest: ")) / 12 / 100
 
-    n_ = number_payments(p_, a_, i_)
+def overpayment(p: int, sum_p: float) -> int:
+    """
+    Returns the calculating overpayment
+    :param p: Loan principal
+    :param sum_p: The sum of all payments
+    :return: Calculating overpayment
+    """
+    return int(sum_p - p)
 
-    if n_ < 12:
-        print(f'It will take {n_} {singular_plural(n_, "month")} to repay this loan!')
+
+def err():
+    """
+    Returns an error
+    :return: String "Incorrect parameters"
+    """
+    print("Incorrect parameters")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(exit_on_error=False)
+
+    parser.add_argument('--type', choices=['diff', 'annuity'])
+    parser.add_argument('--principal', type=int, default=0)
+    parser.add_argument('--periods', type=int, default=0)
+    parser.add_argument('--interest', type=float)
+    parser.add_argument('--payment', type=int, default=0)
+
+    try:
+        args = parser.parse_args()
+
+    except (argparse.ArgumentError, argparse.ArgumentTypeError, TypeError, ValueError):
+        err()
 
     else:
-        y_ = converted_months(n_)
-        if len(y_) == 1:
-            print(f'It will take {y_[0]} {singular_plural(n_, "year")} to repay this loan!')
+        if all([args.interest is not None, len(argv) == 5]):
+            i_ = args.interest / 12 / 100  # Nominal interest rate
+            all_payment = 0
+
+            if all([args.type == 'diff', args.payment == 0]):
+
+                # calculating differentiated payments
+                if all([args.principal > 0, args.periods > 0]):
+                    ind = 1
+                    D = differentiated_payment(args.principal, args.periods, i_)
+                    for el in D:
+                        print(f'Month {ind}: payment is {el}')
+                        ind += 1
+                    print()
+                    all_payment = sum(D)
+
+            elif args.type == 'annuity':
+                # calculating the loan principal
+                if all([args.payment > 0, args.periods > 0]):
+                    args.principal = loan_principal(args.payment, args.periods, i_)
+                    print(f'Your loan principal = {args.principal}!')
+                    all_payment = args.payment * args.periods
+
+                # calculating the monthly payment (the annuity payment)
+                elif all([args.principal > 0, args.periods > 0]):
+                    a_ = annuity_payment(args.principal, args.periods, i_)
+                    print(f'Your annuity payment = {a_}!')
+                    all_payment = a_ * args.periods
+
+                # calculate how long it will take to repay a loan
+                elif all([args.principal > 0, args.payment > 0]):
+                    n_ = number_payments(args.principal, args.payment, i_)
+
+                    if n_ < 12:
+                        print(f'It will take {int(n_)} {singular_plural(n_, "month")} to repay this loan!')
+
+                    else:
+                        y_ = converted_months(n_)
+                        if len(y_) == 1:
+                            print(f'It will take {int(y_[0])} {singular_plural(n_, "year")} to repay this loan!')
+                        else:
+                            year = f'{int(y_[0])} {singular_plural(n_, "year")}'
+                            month = f'{int(y_[1])} {singular_plural(n_, "month")}'
+                            print(
+                                f'It will take {year} and {month} to repay this loan!')
+
+                    all_payment = args.payment * n_
+
+            else:
+                err()
+
+            if all_payment > 0:
+                op = overpayment(args.principal, all_payment)
+                print(f'Overpayment = {op}')
+
         else:
-            year = f'{int(y_[0])} {singular_plural(n_, "year")}'
-            month = f'{int(y_[1])} {singular_plural(n_, "month")}'
-            print(
-                f'It will take {year} and {month} to repay this loan!')
-
-# calculating the monthly payment (the annuity payment)
-elif type_ == 'a':
-    p_ = int(input("Enter the loan principal: "))
-    n_ = int(input("Enter the number of periods: "))
-    i_ = float(input("Enter the loan interest: ")) / 12 / 100
-
-    a_ = annuity_payment(p_, n_, i_)
-
-    print(f'Your monthly payment = {a_}!')
-
-# calculating the loan principal
-elif type_ == 'p':
-    a_ = float(input("Enter the annuity payment: "))
-    n_ = int(input("Enter the number of periods: "))
-    i_ = float(input("Enter the loan interest: ")) / 12 / 100
-
-    p_ = loan_principal(a_, n_, i_)
-
-    print(f'Your loan principal = {p_}!')
+            err()
